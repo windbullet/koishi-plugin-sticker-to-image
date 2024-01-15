e15535e4b6256eb1a0e467e014ee661f846233e5
windbullet <2536810643@qq.com>
2024-01-15 18:33:20 +0800

    提交文件

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, h } from 'koishi'
 
 export const name = 'sticker-to-image'
 
@@ -7,5 +7,21 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.command("表情转图片")
+    .action(async ({ session }) => {
+      if (session.quote === undefined) {
+        await session.send("请在30秒内发送表情")
+        let sticker = await session.prompt(30000)
+        if (!sticker.includes("img"))
+          return "这看上去不是表情"
+        if (sticker !== undefined) {
+          return sticker
+        }
+        
+      } else {
+        if (!session?.quote?.content.includes("img"))
+          return "这看上去不是表情"
+        return session?.quote?.content
+      }
+    })
 }
