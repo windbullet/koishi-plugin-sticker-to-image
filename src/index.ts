e8ce5ed7c46ed849f28a069f672a430e50a34f7b
windbullet <2536810643@qq.com>
2024-08-07 11:01:39 +0800

    支持转换商城表情，优化了转换方式

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Context, Schema, h } from 'koishi'
 
 export const name = 'sticker-to-image'
 
+export const usage = "更新日志：https://forum.koishi.xyz/t/topic/6430"
+
 export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({})
@@ -12,16 +14,44 @@ export function apply(ctx: Context) {
       if (session.quote === undefined) {
         await session.send("请在30秒内发送表情")
         let sticker = await session.prompt(30000)
-        if (!sticker.includes("img"))
+
+        if (!sticker.includes("img") && !sticker.includes("mface")) {
           return "这看上去不是表情"
-        if (sticker !== undefined) {
-          return sticker
         }
+
+        let elements = h.parse(sticker)
+
+        if (h.select(elements, "img").length !== 0) {
+          for (let element of h.select(elements, "img")) {
+            return h.image(element.attrs.src)
+          }
+        } 
+        
+        if (h.select(elements, "mface").length !== 0) {
+          for (let element of h.select(elements, "mface")) {
+            return h.image(element.attrs.url)
+          }
+        }
+
         
       } else {
-        if (!session?.quote?.content.includes("img"))
+        if (!session.quote.content.includes("img") && !session.quote.content.includes("mface")) {
           return "这看上去不是表情"
-        return session?.quote?.content
+        }
+
+        let elements = h.parse(session.quote.content)
+
+        if (h.select(elements, "img").length !== 0) {
+          for (let element of h.select(elements, "img")) {
+            return h.image(element.attrs.src)
+          }
+        } 
+        
+        if (h.select(elements, "mface").length !== 0) {
+          for (let element of h.select(elements, "mface")) {
+            return h.image(element.attrs.url)
+          }
+        }
       }
     })
 }
