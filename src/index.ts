def0552f55515ac0cc0ed10a932d5e176c9592e9
windbullet <2536810643@qq.com>
2025-01-24 19:40:07 +0800

    Fix #1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema, h } from 'koishi'
+import { Context, Schema, h, Session } from 'koishi'
 
 export const name = 'sticker-to-image'
 
@@ -11,47 +11,45 @@ export const Config: Schema<Config> = Schema.object({})
 export function apply(ctx: Context) {
   ctx.command("表情转图片")
     .action(async ({ session }) => {
-      if (session.quote === undefined) {
-        await session.send("请在30秒内发送表情")
-        let sticker = await session.prompt(30000)
-
-        if (!sticker.includes("img") && !sticker.includes("mface")) {
-          return "这看上去不是表情"
-        }
-
-        let elements = h.parse(sticker)
-
-        if (h.select(elements, "img").length !== 0) {
-          for (let element of h.select(elements, "img")) {
-            return h.image(element.attrs.src)
-          }
-        } 
-        
-        if (h.select(elements, "mface").length !== 0) {
-          for (let element of h.select(elements, "mface")) {
-            return h.image(element.attrs.url)
-          }
-        }
-
-        
-      } else {
-        if (!session.quote.content.includes("img") && !session.quote.content.includes("mface")) {
-          return "这看上去不是表情"
-        }
-
-        let elements = h.parse(session.quote.content)
-
-        if (h.select(elements, "img").length !== 0) {
-          for (let element of h.select(elements, "img")) {
-            return h.image(element.attrs.src)
-          }
-        } 
-        
-        if (h.select(elements, "mface").length !== 0) {
-          for (let element of h.select(elements, "mface")) {
-            return h.image(element.attrs.url)
-          }
-        }
-      }
+      await send(session, h.image)
     })
+
+  ctx.command("表情转文件", "gif可用")
+    .action(async ({ session }) => {
+      await send(session, h.file)
+    })
+
+  async function send(session: Session<never, never, Context>, handler: h.Factory<[data: string] | [data: Buffer | ArrayBuffer | ArrayBufferView, type: string]>) {
+    let sticker: string
+
+    if (session.quote === undefined) {
+      await session.send("请在30秒内发送表情")
+      sticker = await session.prompt(30000)
+    } else {
+      sticker = session.quote.content
+    }
+    
+    if (!sticker.includes("img") && !sticker.includes("mface")) {
+      return "这看上去不是表情"
+    }
+
+    let elements = h.parse(sticker)
+
+    for (let element of h.select(elements, "img")) {
+      // let image = await ctx.http("GET", element.attrs.src, { responseType: "arraybuffer" })
+      await session.send(handler(element.attrs.src))
+    }
+  
+    for (let element of h.select(elements, "mface")) {
+      // let image = await ctx.http("GET", element.attrs.src, { responseType: "arraybuffer" })
+      try {
+        await session.send(handler(element.attrs.src))
+      } catch {
+        await session.send(handler(element.attrs.url))
+      }
+      
+    }
+  }
+
+  
 }
